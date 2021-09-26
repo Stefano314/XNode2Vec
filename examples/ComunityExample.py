2c7a4f240aa0f88ee3c2fb4c41afb365c45d4e3d
Stefano <stefanobianchi314@gmail.com>
2021-09-26 23:09:30 +0200

    Changed XNode2Vec to Xnode2vec.

--- examples/ComunityExample.py
+++ examples/ComunityExample.py
@@ -1,4 +1,4 @@
-import XNode2Vec as xn2v
+import Xnode2vec as xn2v
 import numpy as np
 import networkx as nx
 
@@ -9,7 +9,7 @@ p, q = 0.1, 0.9
 starting_node = 4
 nodes, similarity = xn2v.n2v_algorithm(G, starting_node, picked = 6,
                                   dim=128, walk_length=walk_l,
-                                  context=100, p=p, q=q, Weight = False,
+                                  context=5, p=p, q=q, Weight = False,
                                   workers=4, train_time = 5)
 nodes = np.append(nodes, starting_node)
 xn2v.Draw(G, nodes)
