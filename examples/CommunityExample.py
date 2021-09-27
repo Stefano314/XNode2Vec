ba189ff191c1cd94ea99dda98ddc59ab335c091b
Stefano <stefanobianchi314@gmail.com>
2021-09-27 10:06:01 +0200

    Replaced n2v_algorithm with similar_nodes.

--- examples/CommunityExample.py
+++ examples/CommunityExample.py
@@ -7,7 +7,7 @@ G = nx.from_numpy_matrix(G)
 walk_l = 50
 p, q = 0.1, 0.9
 starting_node = 4
-nodes, similarity = xn2v.n2v_algorithm(G, starting_node, picked = 6,
+nodes, similarity = xn2v.similar_nodes(G, starting_node, picked = 6,
                                   dim=128, walk_length=walk_l,
                                   context=5, p=p, q=q, Weight = False,
                                   workers=4, train_time = 5)
