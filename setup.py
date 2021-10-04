f192e971514989c4c0ff8e02512f96c055cb8b6d
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-10-04 18:35:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Xnode2vec",
-    version="0.0.4.2",
+    version="0.0.5",
     author="Stefano Bianchi",
     author_email="stefanobianchi314@gmail.com",
     description="Implementation of fastnode2vec algorithm for networkx objects.",
