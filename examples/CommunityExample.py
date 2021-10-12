60eec7e4e52e5118fa48eab4139880d369680764
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-10-12 10:54:47 +0200

    Used latest Draw() name draw_community().

--- examples/CommunityExample.py
+++ examples/CommunityExample.py
@@ -12,4 +12,4 @@ nodes, similarity = xn2v.similar_nodes(G, starting_node, picked = 6,
                                   context=5, p=p, q=q, Weight = False,
                                   workers=4, train_time = 5)
 nodes = np.append(nodes, starting_node)
-xn2v.Draw(G, nodes)
+xn2v.draw_community(G, nodes)
