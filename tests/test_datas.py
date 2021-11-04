06dbc95c8a87a1ecd27b406d81992959b62909e3
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-11-04 16:56:40 +0100

    Added test.
    Since low_limit_network() has now the possibility to remove links, it has been added a test for that.

--- tests/test_datas.py
+++ tests/test_datas.py
@@ -1,17 +1,9 @@
 import numpy as np
+import pandas as pd
 import networkx as nx
-from Xnode2vec import nx_to_Graph, best_line_projection, low_limit_network
+from Xnode2vec import best_line_projection, low_limit_network
 import pytest
-import fastnode2vec as fn2v
 
-def test_Graph_type():
-    """
-    Tests the object created by the function nx_to_Graph() belongs to the fastnode2vec.Graph class.
-    """
-    G = nx.generators.balanced_tree(2,2)
-    G_fn2v = xn2v.nx_to_Graph(G)
-    assert fn2v.Graph == type(G_fn2v)
-    
 def test_line_points():
     """
     Checks if the number of points returned by the best_line_projection() is the same as the original dataset.
@@ -34,7 +26,7 @@ def test_line_dimension():
     trans_dataset = best_line_projection(dataset)
     assert dataset[0].size == trans_dataset[0].size
 
-def test_low_threshold():
+def test_low_threshold_zeros():
     """
     Checks if all the values below the specified *2.13125* threshold are set to zero.
     """
@@ -42,8 +34,19 @@ def test_low_threshold():
     G = nx.Graph()
     G.add_weighted_edges_from([('1', '2', 3.0), ('1', '3', 7.5), ('1', '4', 3.6), ('2', '4', 1.9),
                                ('4', '5', 1.0), ('2', '6', 5.1), ('2', '7', 1.0), ('3', '7', 11)])
-    G = low_limit_network(G, delta)
+    G = low_limit_network(G, delta, remove=False)
     # Post-Weights
     link_weights_post = nx.get_edge_attributes(G, 'weight')
     weights_post = np.array(list(link_weights_post.values())).astype(float)
     assert np.count_nonzero(weights_post == 0) == 3
+
+def test_low_threshold_remove():
+    """
+    Checks if all the values below the specified *2.13125* threshold are set to zero.
+    """
+    delta = 0.5
+    G = nx.Graph()
+    G.add_weighted_edges_from([('1', '2', 3.0), ('1', '3', 7.5), ('1', '4', 3.6), ('2', '4', 1.9),
+                               ('4', '5', 1.0), ('2', '6', 5.1), ('2', '7', 1.0), ('3', '7', 11)])
+    G = low_limit_network(G, delta, remove=True)
+    assert not any([G.has_edge('2','4'), G.has_edge('4','5'), G.has_edge('2','7')])
