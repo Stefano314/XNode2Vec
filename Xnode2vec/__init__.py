58d15d426606352532ff128e4fa7debbae6b60ae
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-11-09 18:21:33 +0100

    XNode2Vec file splitting into smaller ones.

--- Xnode2vec/__init__.py
+++ Xnode2vec/__init__.py
@@ -1,3 +1,3 @@
-from .XNode2Vec import similar_nodes, cluster_generation, clusters_detection, low_limit_network
-from .XNode2Vec import load_model, draw_community, nx_to_Graph, labels_modifier
-from .XNode2Vec import generate_edgelist, edgelist_from_csv, complete_edgelist, stellar_edgelist, best_line_projection, recover_points
+from .data_clusterization import similar_nodes, cluster_generation, clusters_detection, low_limit_network
+from .data_management import load_model, draw_community, nx_to_Graph, labels_modifier
+from .data_manipulation import generate_edgelist, edgelist_from_csv, complete_edgelist, stellar_edgelist, best_line_projection, recover_points
