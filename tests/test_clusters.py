e8e567b9f4928aa2eb4bfa39a1fd7da8ec1f107b
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-11-10 15:28:29 +0100

    Added test_similar_nodes_picked().

--- tests/test_clusters.py
+++ tests/test_clusters.py
@@ -44,6 +44,18 @@ def test_similar_nodes_dimensions():
     nodes, similarities = similar_nodes(G,node=rand_node,context=5,dim=100,walk_length=int(len(list(G.nodes))/2))
     assert nodes.size == similarities.size
 
+def test_similar_nodes_picked():
+    """
+    Description
+    -----------
+    Test of similar_nodes() function.
+    Checks if the 'picked' parameter returns a vector of dimension 'picked'.
+    """
+    G = nx.generators.balanced_tree(r=3, h=3)
+    rand_picked = np.random.randint(0,10)
+    nodes, similarities = similar_nodes(G, node=1, picked = rand_picked, context=5, dim=40, walk_length=5)
+    assert nodes.size == rand_picked
+
 def test_similar_nodes_community():
     """
     Description
