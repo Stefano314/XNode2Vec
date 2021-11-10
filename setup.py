02ed1cfcb514b4442a875ebdee86c7a867940266
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2021-11-10 09:54:33 +0100

    v 0.11.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Xnode2vec",
-    version="0.11.3",
+    version="0.11.4",
     author="Stefano Bianchi",
     author_email="stefanobianchi314@gmail.com",
     description="Alternative method for data clustering using Node2Vec algorithm.",
